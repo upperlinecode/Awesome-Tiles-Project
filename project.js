a7275dc410da8e676ee1d949caad2029e15d6b72
Dayanna Larrea <dl1380@uhhsnyc.org>
2018-02-23 18:25:49 +0000

    project deploy

---
+++ project.js
@@ -0,0 +1,117 @@
+let sq1 = document.querySelector('.square1')
+    console.log(sq1)
+        sq1.addEventListener("mouseover", e =>{
+            sq1.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq1.classList.remove('rotate')
+        },3000)
+        })
+
+
+let sq2 = document.querySelector('.square2')
+    console.log(sq2)
+        sq2.addEventListener("mouseover", e =>{
+            sq2.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq2.classList.remove('rotate')
+        },3000)
+        })
+        
+let sq3 = document.querySelector('.square3')
+    console.log(sq3)
+        sq3.addEventListener("mouseover", e =>{
+            sq3.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq3.classList.remove('rotate')
+        },3000)
+        })
+
+let sq4 = document.querySelector('.square4')
+    console.log(sq4)
+        sq4.addEventListener("mouseover", e =>{
+            sq4.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq4.classList.remove('rotate')
+        },3000)
+        })
+
+let sq5 = document.querySelector('.square5')
+    console.log(sq5)
+        sq5.addEventListener("mouseover", e =>{
+            sq5.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq5.classList.remove('rotate')
+        },3000)
+        })
+
+let sq6 = document.querySelector('.square6')
+    console.log(sq6)
+        sq6.addEventListener("mouseover", e =>{
+            sq6.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq6.classList.remove('rotate')
+        },3000)
+        })
+
+let sq7 = document.querySelector('.square7')
+    console.log(sq7)
+        sq7.addEventListener("mouseover", e =>{
+            sq7.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq7.classList.remove('rotate')
+        },3000)
+        })
+
+let sq8 = document.querySelector('.square8')
+    console.log(sq8)
+        sq8.addEventListener("mouseover", e =>{
+            sq8.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq8.classList.remove('rotate')
+        },3000)
+        })
+        
+let sq9 = document.querySelector('.square9')
+    console.log(sq9)
+        sq9.addEventListener("mouseover", e =>{
+            sq9.classList.add('rotate')
+            sq1.style.backgroundColor = "lightblue"
+            setTimeout(e => {
+                sq1.style.backgroundColor = "darkblue"
+            },2000)
+            setTimeout(e => {
+            sq9.classList.remove('rotate')
+        },3000)
+        })
